10d4f63b38c686a35436c4a224aabe95e2fb068d
Ju-wang <80758618+Ju-wang@users.noreply.github.com>
2024-02-11 01:16:42 +0900

    2/10

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,22 +4,41 @@ export const home = async (req, res) => {
     const videos = await Video.find({})
     return res.render("home", {pageTitle: "Home", videos})
 }
-export const handleWatch = (req, res) => {
+export const handleWatch = async (req, res) => {
     const id = req.params.id
-    return res.render("watch", {
-        pageTitle: `Watching`
-    })
+    const video = await Video.findById(id).exec()
+    if(video){
+        return res.render("watch", {pageTitle:video.title, video})
+    }else{
+        return res.render("404", {pageTitle:"Nothing Found."})
+    }
 }
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
     const id = req.params.id
-    return res.render("edit", {
-        pageTitle: `Editing`,
-    })
+    const video = await Video.findById(id).exec()
+    if(video){
+        return res.render("edit", {
+            pageTitle: `Editing ${video.title}`,
+            video
+        })
+    }else{
+        return res.render("404", {pageTitle:"Nothing Found."})
+    }
 }
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
     const id = req.params.id
-    const title = req.body.title
-    return res.redirect(`/videos/${id}`)
+    const { title, description, hashtags}  = req.body
+    const video = await Video.findById(id).exec()
+    if(video){
+        await Video.findByIdAndUpdate(id, {
+            title,
+            description,
+            hashtags
+        })
+        return res.redirect(`/videos/${id}`)
+    } else{
+        return res.render("404")
+    }  
 }
 
 export const getUpload = (req,res) => {
@@ -28,19 +47,21 @@ export const getUpload = (req,res) => {
     })
 }
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
     const {title, description, hashtags} = req.body
-    const video = new Video({
-        title,
-        description,
-        createdAt: Date.now(),
-        hashtags: hashtags.split(",").map((word) => `#${word}`),
-        meta:{
-            views: 0,
-            rating: 0
-        }
-    })
-    console.log(video);
-    return res.redirect("/")
+    try{
+        await Video.create({
+            title,
+            description,
+            hashtags: hashtags.split(",").map((word) => `#${word}`),
+        })
+        return res.redirect("/")
+    } catch(error){
+        console.log(error);
+        return res.render("upload", {
+            pageTitle: "Upload Video"
+        })
+    }
+        
 }
  
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,9 +4,9 @@ import { handleWatch, getEdit, postEdit, getUpload, postUpload } from "../contro
 export const videoRouter = express.Router()
 // export const videoCommentRouter = express.Router()
 
-videoRouter.get("/:id(\\d+)/edit", getEdit)
-videoRouter.post("/:id(\\d+)/edit", postEdit)
-videoRouter.get("/:id(\\d+)", handleWatch)
+videoRouter.get("/:id([0-9a-f]{24})/edit", getEdit)
+videoRouter.post("/:id([0-9a-f]{24})/edit", postEdit)
+videoRouter.get("/:id([0-9a-f]{24})", handleWatch)
 videoRouter.get("/upload", getUpload)
 videoRouter.post("/upload", postUpload)
 
