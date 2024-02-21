6bc8c5b91f12ceec1fa37c42dd597edfb36d1cf1
Ju-wang <80758618+Ju-wang@users.noreply.github.com>
2024-02-22 01:33:41 +0900

    2/21

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -52,6 +52,37 @@ export const postLogin = async (req, res) => {
     req.session.user = savedUser.username
     return res.redirect("/")
 }
+
+export const handleGithubLogin = (req, res) => {
+    const baseUrl = `https://github.com/login/oauth/authorize`
+    const config = {
+        client_id: process.env.GH_CLIENT_ID,
+        scope: "read:user user:email"
+    }
+    const params = new URLSearchParams(config).toString()
+    const finalUrl = `${baseUrl}?${params}`
+    return res.redirect(finalUrl)
+}
+
+export const handleGithubCallback = async (req, res) => {
+    const baseUrl = "https://github.com/login/oauth/access_token"
+    const config = {
+        client_id: process.env.GH_CLIENT_ID,
+        client_secret: process.env.GH_SECRET,
+        code: req.query.code
+    }
+    const params = new URLSearchParams(config).toString()
+    const finalUrl = `${baseUrl}?${params}`
+    const response = await fetch(finalUrl, {
+        method: "POST",
+        headers: {
+            Accept: "application/json"
+        }
+    }) 
+    const data = await response.json()
+    console.log(data);
+}
+
 export const logout = (req, res) => res.send("log out")
 export const handleEdit = (req, res) => res.send("Edit User")
 export const handleRemove = (req, res) => res.send("Remove User")
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { handleEdit, handleRemove, logout, handleSee } from "../controllers/userController"
+import { handleEdit, handleRemove, logout, handleSee, handleGithubLogin, handleGithubCallback } from "../controllers/userController"
 
 const userRouter = express.Router()
 
@@ -8,7 +8,8 @@ userRouter.get("/edit", handleEdit)
 userRouter.get("/remove", handleRemove)
 userRouter.get("/logout", logout)
 userRouter.get(":id(\\d+)", handleSee)
-
+userRouter.get("/github/start", handleGithubLogin)
+userRouter.post("/github/callback", handleGithubCallback)
 
 
 
